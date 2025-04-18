6f8d98cf16b3068513eca23510590e53378fa86a
Northern-llights <northernlightss001@gmail.com>
2025-04-18 13:17:50 -0700

    Sorting Bots

---
+++ SortBar.js
@@ -0,0 +1,11 @@
+function SortBar({ sortBots }) {
+  return (
+    <div className="sort-bar">
+      <button onClick={() => sortBots("health")}>Sort by Health</button>
+      <button onClick={() => sortBots("damage")}>Sort by Damage</button>
+      <button onClick={() => sortBots("armor")}>Sort by Armor</button>
+    </div>
+  );
+}
+
+export default SortBar;
